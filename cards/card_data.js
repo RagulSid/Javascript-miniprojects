6d45aa27e0d11fe8b7fd48058e68c1975a0f9b71
RagulSid <ragulsiddharth1@gmail.com>
2022-12-27 14:55:37 +0530

    arrays -> array of objects

--- cards/card_data.js
+++ cards/card_data.js
@@ -1,23 +1,32 @@
-var titlearray =["js","python","java","android","jquery","ruby"];
-var descriptionarray =["js program","python code","java objects","android program","jquery objects","ruby code"];
+const langs = [
+  {title: "js", description:"js program"},
+  {title: "python", description:"python code"},
+  {title: "java", description:"java objects"},
+  {title: "android", description:"android program"},
+  {title: "jquery", description:"jquery objects"},
+  {title: "ruby", description:"ruby code"}
+]
 
-var dynamic = document.querySelector('.container');  
-for (var i = 0; i < titlearray.length; i++) {
+var dynamic = document.querySelector('.container');
+
+for (var i = 0; i < langs.length; i++) {
   var fetch = document.querySelector('.container').innerHTML;  
   dynamic.innerHTML = `<div id="card${i}" class="boxes">
       <div class="box-content">
-        <h2>${titlearray[i]}</h2>
+        <h2>${langs[i].title}</h2>
         <p>
-          ${descriptionarray[i]}
+          ${langs[i].description}
         </p>
         <a class="showmore" href="#">Read More</a>
       </div>
     </div>` + fetch ; 
     var bgimg = document.getElementById(`card${i}`);
-    bgimg.style.backgroundImage = `url('img/${titlearray[i]}.jpg')`;
+    bgimg.style.backgroundImage = `url('img/${langs[i].title}.jpg')`;
 } 
 
-
-const buttonPressed = e => {
-  console.log(e.target.);
+function cardinfo(id){
+    const fullnames = langs.map(x => x.title + x.description);
+    console.log(fullnames);
 }
+
+// const fullnames = users.filter((x) => x.title == "js").map((x) => x.description);
